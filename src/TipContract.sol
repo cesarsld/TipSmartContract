036295fb14d8cd6b69de4877b14454b85734f139
cesarsld <jaimes.cesar88@gmail.com>
2020-11-07 16:39:44 +0100

    Refactor to simpler contract

--- src/TipContract.sol
+++ src/TipContract.sol
@@ -4,58 +4,54 @@ import "./ownable.sol";
 import "./IERC20.sol";
 import "./SafeMath.sol";
 
-contract TipContract is Ownable
-{
+contract TipContract is Ownable {
 	using SafeMath for uint256;
 
 	uint256 public etherBalance;
-	mapping(address => uint256) public balances;
 	mapping(address => uint256) public fees;
+	mapping(address => bool) whitelistedTokens;
 
-	event DiscordDeposit(address tokenContract, uint256 amount, uint64 indexed discordId);
-	event DiscordWithdrawal(address tokenContract, uint256 amount, address recipient, uint64 indexed discordId);
+	event DiscordDeposit(address indexed tokenContract, uint256 amount, uint64 indexed discordId);
+	event DiscordWithdrawal(address indexed tokenContract, uint256 amount, address recipient, uint64 indexed discordId);
 
-	function depositEther(uint64 discordId) external payable
-	{
+	function updateTokens(address[] calldata _tokens, bool _value) external onlyOwner {
+		for (uint256 i = 0; i < _tokens.length; i++)
+			whitelistedTokens[_tokens[i]]  =  _value;
+	}
+
+	function depositEther(uint64 discordId) external payable {
 		etherBalance = etherBalance.add(msg.value);
 		emit DiscordDeposit(address(0), msg.value, discordId);
 	}
 
-	function withdrawEther(uint256 amount, uint256 fee, address payable recipient, uint64 discordId) public onlyOperator
-	{
-		require(etherBalance > amount.add(fee), "Unsufficient funds.");
+	function withdrawEther(uint256 amount, uint256 fee, address payable recipient, uint64 discordId) public onlyOperator {
 		etherBalance = etherBalance.sub(fee);
 		etherBalance = etherBalance.sub(amount);
-		recipient.transfer(amount);
-		emit  DiscordWithdrawal(address(0), amount, recipient, discordId);
+		recipient.transfer(amount.sub(fee));
+		emit  DiscordWithdrawal(address(0), amount.sub(fee), recipient, discordId);
 	}
 
-	function depositToken(uint256 amount, address tokenContract, uint64 discordId) external
-	{
-		require (IERC20(tokenContract).transferFrom(msg.sender, address(this), amount), "Transfer from function failed.");
-		balances[tokenContract] = balances[tokenContract].add(amount);
+	function depositToken(uint256 amount, address tokenContract, uint64 discordId) external {
+		require(whitelistedTokens[tokenContract], "TipContract: Token not whitelisted.");
+		IERC20(tokenContract).transferFrom(msg.sender, address(this), amount);
 		emit DiscordDeposit(tokenContract, amount, discordId);
 	}
 
-	function withdrawToken(uint256 amount, uint256 fee, address tokenContract, address recipient, uint64 discordId) public onlyOperator
-	{
-		require (IERC20(tokenContract).transfer(recipient, amount.sub(fee)), "Transfer function failed.");
-		balances[tokenContract] = balances[tokenContract].sub(amount.add(fee));
+	function withdrawToken(uint256 amount, uint256 fee, address tokenContract, address recipient, uint64 discordId) public onlyOperator {
 		fees[tokenContract] = fees[tokenContract].add(fee);
-		emit DiscordWithdrawal(tokenContract, amount, recipient, discordId);
+		IERC20(tokenContract).transfer(recipient, amount.sub(fee)
+		emit DiscordWithdrawal(tokenContract, amount.sub(fee), recipient, discordId);
 	}
 
-	function withdrawFees(address recipient, address tokenContract) public onlyOwner returns (bool)
-	{
-		require (fees[tokenContract] > 0, "Balance is empty.");
-		require (IERC20(tokenContract).transfer(recipient, fees[tokenContract]), "Transfer function failed.");
+	function withdrawFees(address recipient, address tokenContract) public onlyOwner returns (bool) {
+		require (fees[tokenContract] > 0, "TipContract: Balance is empty.");
+		uint256 fee = fees[tokenContract];
 		fees[tokenContract] = 0;
-		return true;
+		return IERC20(tokenContract).transfer(owner, fee);
 	}
 
-	function syphonEther(uint256 amount) public onlyOperator
-	{
-		require (amount < address(this).balance.sub(etherBalance), "Amount is higher than ether balance");
+	function syphonEther(uint256 amount) public onlyOperator {
+		require (amount < address(this).balance.sub(etherBalance), "TipContract: Amount is higher than ether balance");
 		owner.transfer(amount);
 	}
 }
--- src/ownable.sol
+++ src/ownable.sol
@@ -1,41 +1,53 @@
 pragma solidity ^0.6.4;
 
-contract Ownable
-{
-	address payable public owner;
-	address public operator;
+contract TipContract is Ownable {
+	using SafeMath for uint256;
 
-	constructor() public
-	{
-		owner = msg.sender;
-		operator = msg.sender;
+	uint256 public etherBalance;
+	mapping(address => uint256) public fees;
+	mapping(address => bool) whitelistedTokens;
+
+	event DiscordDeposit(address indexed tokenContract, uint256 amount, uint64 indexed discordId);
+	event DiscordWithdrawal(address indexed tokenContract, uint256 amount, address recipient, uint64 indexed discordId);
+
+	function updateTokens(address[] calldata _tokens, bool _value) external onlyOwner {
+		for (uint256 i = 0; i < _tokens.length; i++)
+			whitelistedTokens[_tokens[i]]  =  _value;
 	}
 
-	modifier onlyOwner()
-	{
-		require(msg.sender == owner,
-		"Sender not authorised to access.");
-		_;
+	function depositEther(uint64 discordId) external payable {
+		etherBalance = etherBalance.add(msg.value);
+		emit DiscordDeposit(address(0), msg.value, discordId);
 	}
 
-	modifier onlyOperator()
-	{
-		require(msg.sender == owner || msg.sender == operator,
-		"Sender not authorised to access.");
-		_;
+	function withdrawEther(uint256 amount, uint256 fee, address payable recipient, uint64 discordId) public onlyOperator {
+		etherBalance = etherBalance.sub(fee);
+		etherBalance = etherBalance.sub(amount);
+		recipient.transfer(amount.sub(fee));
+		emit  DiscordWithdrawal(address(0), amount.sub(fee), recipient, discordId);
 	}
 
-	function transferOwnership(address payable newOwner) external onlyOwner
-	{
-        if (newOwner != address(0)) {
-            owner = newOwner;
-        }
-    }
+	function depositToken(uint256 amount, address tokenContract, uint64 discordId) external {
+		require(whitelistedTokens[tokenContract], "TipContract: Token not whitelisted.");
+		IERC20(tokenContract).transferFrom(msg.sender, address(this), amount);
+		emit DiscordDeposit(tokenContract, amount, discordId);
+	}
 
-	function transferOperatorRights(address payable newOperator) external onlyOwner
-	{
-        if (newOperator != address(0)) {
-            operator = newOperator;
-        }
-    }
+	function withdrawToken(uint256 amount, uint256 fee, address tokenContract, address recipient, uint64 discordId) public onlyOperator {
+		fees[tokenContract] = fees[tokenContract].add(fee);
+		IERC20(tokenContract).transfer(recipient, amount.sub(fee));
+		emit DiscordWithdrawal(tokenContract, amount.sub(fee), recipient, discordId);
+	}
+
+	function withdrawFees(address tokenContract) public onlyOwner returns (bool) {
+		require (fees[tokenContract] > 0, "TipContract: Balance is empty.");
+		uint256 fee = fees[tokenContract];
+		fees[tokenContract] = 0;
+		return IERC20(tokenContract).transfer(owner, fee);
+	}
+
+	function syphonEther(uint256 amount) public onlyOperator {
+		require (amount < address(this).balance.sub(etherBalance), "TipContract: Amount is higher than ether balance");
+		owner.transfer(amount);
+	}
 }
